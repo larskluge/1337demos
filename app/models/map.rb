9501ca56c9120036c9d898b49529ace23540d758
lars <lars@093dad87-8a4d-4dfd-829b-36c79ddf6f75>
2008-07-19 00:35:47 +0000

    map dependency on acts_as_rated removed
    git-svn-id: svn+ssh://1337demos.com/home/lars/.svnrepo/1337demos/trunk@2 093dad87-8a4d-4dfd-829b-36c79ddf6f75

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,7 +1,7 @@
 class Map < ActiveRecord::Base
 	has_many :demos
 
-	acts_as_rated :no_rater => true
+	#acts_as_rated :no_rater => true
 
 	validates_presence_of :name
 	validates_uniqueness_of :name
