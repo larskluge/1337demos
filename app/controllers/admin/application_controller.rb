e16d638ed13ec30b5b777a4bdd0e5856270ada71
lars <lars@093dad87-8a4d-4dfd-829b-36c79ddf6f75>
2008-07-19 14:29:38 +0000

    admin application controller added
    git-svn-id: svn+ssh://1337demos.com/home/lars/.svnrepo/1337demos/trunk@12 093dad87-8a4d-4dfd-829b-36c79ddf6f75

---
+++ app/controllers/admin/application_controller.rb
@@ -0,0 +1,16 @@
+class Admin::ApplicationController < ApplicationController
+	before_filter :authenticate
+
+
+	layout 'admin/application'
+
+
+	protected
+
+	def authenticate
+		authenticate_or_request_with_http_basic do |user, pass|
+			user == 'admin' && Digest::MD5.hexdigest(pass) == "9c383927da6add23aa278e24eeaa5275"
+		end
+	end
+end
+
