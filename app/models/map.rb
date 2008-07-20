75337efb0d9ea9740ddb4cac9ccd3bcea4cdf254
lars <lars@093dad87-8a4d-4dfd-829b-36c79ddf6f75>
2008-07-20 19:58:46 +0000

    fixed: map list wrong #1
    git-svn-id: svn+ssh://1337demos.com/home/lars/.svnrepo/1337demos/trunk@22 093dad87-8a4d-4dfd-829b-36c79ddf6f75

--- app/models/map.rb
+++ app/models/map.rb
@@ -5,4 +5,14 @@ class Map < ActiveRecord::Base
 
 	validates_presence_of :name
 	validates_uniqueness_of :name
+
+
+
+	def get_first_player
+		demos = self.demos.map { |d| d if d.gamemode == 'race' }.compact
+		return if demos.empty?
+
+		demos.sort { |x,y| x.time <=> y.time }.first.players.first
+	end
+
 end
