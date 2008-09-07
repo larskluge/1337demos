d2b036a8f01fcc7a651f850e95c7f9eeb123ddef
lars <lars@093dad87-8a4d-4dfd-829b-36c79ddf6f75>
2008-09-07 16:14:03 +0000

    Added: demo listing by gamemode race+freestyle
    git-svn-id: svn+ssh://1337demos.com/home/lars/.svnrepo/1337demos/trunk@64 093dad87-8a4d-4dfd-829b-36c79ddf6f75

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -1,73 +1,77 @@
 class Demo < ActiveRecord::Base
 
-	include FlexRating
+  include FlexRating
 
-	#belongs_to :player
-	has_and_belongs_to_many :players
-	belongs_to :map
-	belongs_to :demofile, :dependent => :destroy
-	has_many :comments, :as => :commentable, :dependent => :destroy, :order => 'created_at ASC'
+  #belongs_to :player
+  has_and_belongs_to_many :players
+  belongs_to :map
+  belongs_to :demofile, :dependent => :destroy
+  has_many :comments, :as => :commentable, :dependent => :destroy, :order => 'created_at ASC'
 
-	validates_presence_of :map_id
-	validates_presence_of :demofile_id
+  validates_presence_of :map_id
+  validates_presence_of :demofile_id
+  validates_presence_of :title, :on => :update, :if => Proc.new{|demo| demo.gamemode == 'freestyle'}
+  validates_associated :players, :on => :update
 
-	validates_presence_of :title, :on => :update, :if => Proc.new{|demo| demo.gamemode == 'freestyle'}
+  def validate
+    #logger.info "status: #{self.status}"
+    errors.add(:status, 'can not be "rendered" without an existing video file') if self.status == :rendered && !File.exists?(self.video_filename)
+    #logger.info "error cnt: #{errors.count}"
+  end
 
-	validates_associated :players, :on => :update
+  def validate_on_update
+    errors.add(:data_correct, "can't be empty") if self.data_correct.nil?
+  end
 
-	def validate
-		#logger.info "status: #{self.status}"
-		errors.add(:status, 'can not be "rendered" without an existing video file') if self.status == :rendered && !File.exists?(self.video_filename)
-		#logger.info "error cnt: #{errors.count}"
-	end
 
-	def validate_on_update
-		errors.add(:data_correct, "can't be empty") if self.data_correct.nil?
-	end
 
-	def position
-		return nil if self.gamemode != 'race'
+  named_scope :race, :conditions => 'data_correct AND gamemode = "race"'
 
-		pos = 0
-		demos = self.map.demos
-		return 1 if demos.length == 1
 
-		demos = demos.sort { |x,y| x.time <=> y.time }.collect {|demo| demo unless demo.time == self.time && demo != self}.compact
 
-		# just consider best time of each player
-		known_players = []
-		demos = demos.inject([]) do |list, demo|
-			if demo.gamemode == 'race' && !known_players.include?(demo.players.first)
-				known_players << demo.players.first
-				list << demo
-			end
-			list
-		end
+  def position
+    return nil if self.gamemode != 'race'
 
-		res = demos.index(self)
-		res.nil? ? nil : 1 + res
-	end
+    pos = 0
+    demos = self.map.demos
+    return 1 if demos.length == 1
 
-	def video_width
-		384
-	end
-	def video_height
-		288
-	end
+    demos = demos.sort { |x,y| x.time <=> y.time }.collect {|demo| demo unless demo.time == self.time && demo != self}.compact
 
-	def video_exists?
-		self.status == :rendered && File.exists?(self.video_filename)
-	end
+    # just consider best time of each player
+    known_players = []
+    demos = demos.inject([]) do |list, demo|
+      if demo.gamemode == 'race' && !known_players.include?(demo.players.first)
+        known_players << demo.players.first
+        list << demo
+      end
+      list
+    end
 
-	def video_filename
-		"#{SYS_VIDEOS}#{self.id}.mp4"
-	end
+    res = demos.index(self)
+    res.nil? ? nil : 1 + res
+  end
 
-	def video_url
-		return "#{WEB_VIDEOS}#{self.id}.mp4" if self.video_exists?
-	end
+  def self.video_width
+    384
+  end
+  def self.video_height
+    288
+  end
 
-	def video_file
-		self.id.to_s + ".mp4"
-	end
+  def video_exists?
+    self.status == :rendered && File.exists?(self.video_filename)
+  end
+
+  def video_filename
+    "#{SYS_VIDEOS}#{self.id}.mp4"
+  end
+
+  def video_url
+    return "#{WEB_VIDEOS}#{self.id}.mp4" if self.video_exists?
+  end
+
+  def video_file
+    self.id.to_s + ".mp4"
+  end
 end
