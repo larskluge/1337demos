2cd6326069fea803d76d84e65879ff0dc783df88
lars <lars@093dad87-8a4d-4dfd-829b-36c79ddf6f75>
2008-12-04 23:57:32 +0000

    Add validations from shouts to comments
    git-svn-id: svn+ssh://1337demos.com/home/lars/.svnrepo/1337demos/trunk@81 093dad87-8a4d-4dfd-829b-36c79ddf6f75

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,18 @@ class Comment < ActiveRecord::Base
 	belongs_to :commentable, :polymorphic => :true
 
 	validates_presence_of :name, :message
+  validates_length_of :message, :maximum => 255
+  validate :prevent_url_posting
+
+  def prevent_url_posting
+    if message =~ /[a-zA-Z0-9\-\.]+\.(com|edu|gov|mil|net|org|biz|info|name|museum|us|ca|uk|de)/
+      errors.add_to_base("No urls allowed")
+    end
+  end
+
+
+
+
 
 	def demo
 		return null if self.commentable_type != 'Demo'
