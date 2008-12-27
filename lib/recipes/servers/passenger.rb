4253683dc52ce6febeca953120ccff9b5cec04fc
Lars Kluge <lars.kluge@samedi.de>
2008-12-27 01:11:37 +0100

    Add passenger deploy recipe

---
+++ lib/recipes/servers/passenger.rb
@@ -0,0 +1,15 @@
+namespace :deploy do
+  desc "Restart the app"
+  task :restart, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Start the app"
+  task :start, :roles => :app do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  desc "Stop the app"
+  task :stop, :roles => :app do
+  end
+end
