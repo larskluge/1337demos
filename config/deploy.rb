34d9cf9ab4a03a79a83827a4e60c0ebbca21a31f
Lars Kluge <lars.kluge@samedi.de>
2008-12-27 03:21:45 +0100

    Configure capistrano for deployment on srcbox.net

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,47 @@
-set :application, "1337demos"
-set :repository,  "svn+ssh://lars@1337demos.com/home/lars/.svnrepo/1337demos/trunk"
+require 'capistrano_colors'
+load "lib/recipes/servers/passenger.rb"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
-set :deploy_to, "/var/www/1337demos.com/1337demos_production"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+set :application, '1337demos'
 
-role :app, "1337demos.com"
-role :web, "1337demos.com"
-role :db,  "1337demos.com", :primary => true
 
-set :user, "lars"
-set :deploy_via, :checkout    # default checkout
+# ssh
+set :user, 'lars'
+set :repository,  "ssh://#{user}@1337demos.com/home/lars/git/1337demos.git"
+ssh_options[:forward_agent] = true
+
+
+# git
+set :scm, 'git'
+set :branch, 'master'
+set :scm_verbose, true # to support older git versions on server
+
+
+# remote server
+set :deploy_to, "/var/www/1337demos.com/#{application}"
+set :deploy_via, :remote_cache    # default checkout
+set :rails_env, (ENV['RAILS_ENV'] || 'production')
+set :use_sudo, false
+set :keep_releases, 3
+set :rake, '/var/lib/gems/1.8/bin/rake'
+
+role :app, '1337demos.com'
+role :web, '1337demos.com'
+role :db,  '1337demos.com', :primary => true
+
+
+
+# symlink shared static files to new release
+task :after_update_code, :roles => :app do
+  run "sudo chown www-1337demos #{release_path}/config/environment.rb"
+
+
+
+  static_dirs = %w(data/maps/images public/demofiles public/videos public/images/maps)
+  static_path = "#{deploy_to}/shared/static"
+
+  static_dirs.each do |dir|
+    run "ln -nfs #{static_path}/#{dir} #{release_path}/#{dir}"
+  end
+end
 
