715a1bc62547aa5b1c7a44b41256466c987224f7
Lars Kluge <lars.kluge@samedi.de>
2009-02-07 17:49:30 +0100

    Fix show of admin stuff

--- app/controllers/admin/stuffs_controller.rb
+++ app/controllers/admin/stuffs_controller.rb
@@ -4,6 +4,8 @@ class Admin::StuffsController < Admin::ApplicationController
     list.columns = [:name, :filename, :size, :comment, :created_at]
     list.per_page = 50
     list.sorting = {:created_at => 'DESC'}
+
+    show.columns = [:name, :filename, :size, :comment, :content_type, :created_at]
   end
 
 end
