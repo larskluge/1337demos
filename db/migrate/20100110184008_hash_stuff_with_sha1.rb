717a83874cd2c60e0459ab08cda7da1454889153
aekym <me@aekym.com>
2010-01-10 21:25:23 +0100

    wrap migration into transaction

--- db/migrate/20100110184008_hash_stuff_with_sha1.rb
+++ db/migrate/20100110184008_hash_stuff_with_sha1.rb
@@ -1,21 +1,26 @@
 class HashStuffWithSha1 < ActiveRecord::Migration
   def self.up
-    add_column :stuffs, :sha1, :string, :limit => 40
+    ActiveRecord::Base.transaction do
+      puts `whoami`
+      add_column :stuffs, :sha1, :string, :limit => 40
 
-    Stuff.find_each do |stuff|
-      stuff.update_attributes!(:sha1 => Digest::SHA1::hexdigest(File.read(stuff.full_filename)))
+      Stuff.find_each do |stuff|
+        stuff.update_attributes!(:sha1 => Digest::SHA1::hexdigest(File.read(stuff.full_filename)))
+      end
+
+      add_index :stuffs, :sha1
+
+      remove_column :stuffs, :md5
     end
-
-    add_index :stuffs, :sha1
-
-    remove_column :stuffs, :md5
   end
 
   def self.down
-    add_column :stuffs, :md5, :string
+    ActiveRecord::Base.transaction do
+      add_column :stuffs, :md5, :string
 
-    remove_index :stuffs, :column => :sha1
-    remove_column :stuffs, :sha1
+      remove_index :stuffs, :column => :sha1
+      remove_column :stuffs, :sha1
+    end
   end
 end
 
