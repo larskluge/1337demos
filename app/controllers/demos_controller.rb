5a9fb6830e32dfc7b774b63808fd9a9e7894f928
aekym <me@aekym.com>
2010-01-10 18:37:27 +0100

    clean up demos_controller

--- app/controllers/demos_controller.rb
+++ app/controllers/demos_controller.rb
@@ -142,30 +142,6 @@ class DemosController < ApplicationController
       end
     end
 
-=begin
-    if params[:demo][:player]
-      nickname = Nickname.find_by_nickname params[:demo][:player]
-      if nickname
-        params[:demo][:player] = nickname.player
-      else
-        begin
-          Player.transaction do
-            Nickname.transaction do
-              player = Player.create!
-              nickname = player.create_in_nicknames :nickname => params[:demo][:player], :player => player
-              player.main_nickname = nickname
-              player.save!
-              params[:demo][:player] = nickname.player
-            end
-          end
-        rescue Exception => e
-          @demo.errors.add_to_base e.message
-          params[:demo][:player] = nil
-        end
-      end
-    end
-=end
-
     if @demo.update_attributes(params[:demo])
       flash[:notice] = 'Demo was successfully uploaded.'
       redirect_to :action => 'show', :id => @demo
@@ -192,19 +168,5 @@ class DemosController < ApplicationController
     end
   end
 
-
-
-=begin
-  def resolutions
-    res = []
-    (4..1024).each do |w|
-      (3..768).each do |h|
-        if w % 16 == 0 && h % 16 == 0 && ((w.to_f / h.to_f) - 4.0/3.0).abs <= Float::EPSILON
-          res << "#{w}x#{h}"
-        end
-      end
-    end
-    render :text => res.join('<br />'), :layout => true
-  end
-=end
 end
+
