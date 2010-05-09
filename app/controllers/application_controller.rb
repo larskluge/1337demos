bed47f121867248c37f87d999947fbd7389a19c4
aekym <me@aekym.com>
2010-05-09 20:18:01 +0200

    try to skip exception notifications on ActionController::MethodNotAllowed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  include ExceptionNotifiable
-
 	# make flash messages cachable via cookie + js
 	include CacheableFlash
 
@@ -16,37 +14,21 @@ class ApplicationController < ActionController::Base
 	layout 'layout_2008-07'
 
 
+  # exception_notification
+  #
+  include ExceptionNotifiable
 
-	# extend caches_action to use a default cache path
-	#alias true_caches_action caches_action
-    #
-	#def caches_action(*actions)
-	#	options = actions.extract_options!
-	#	options[:cache_path] = :cache_path.to_proc
-	#	true_caches_action actions, options
-	#end
-
-	# extend cache 'expire_action' to make it work with and without layout rendering
-	alias true_expire_action expire_action
-
-	def expire_action(options = {})
-		options = {} if options.nil?
-
-		true_expire_action(options.merge({:layout => 'true'}))
-		true_expire_action(options.merge({:layout => 'false'}))
-	end
-
+  def self.exceptions_to_treat_as_404
+    super << ActionController::MethodNotAllowed
+  end
 
 
 	protected
-
-	def cache_path
-		"#{controller_name}/#{action_name}/#{params[:page] || 1}"
-	end
 
   def page_param
     page = params[:page].to_i
     page > 0 ? page : 1
   end
+
 end
 
