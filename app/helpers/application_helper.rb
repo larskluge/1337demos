780a5dc4853c4a1fb2693478fa903771ad59deed
aekym <me@aekym.com>
2010-08-04 23:00:11 +0200

    dynamic pluralize player(s) label for demo#show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -200,5 +200,13 @@ module ApplicationHelper
 
     [demo.game, gameversion, demo.gamemode].join(" ")
   end
+
+
+  def pluralize_without_count(count, noun, text = nil)
+    if count != 0
+      count == 1 ? "#{noun}#{text}" : "#{noun.pluralize}#{text}"
+    end
+  end
+
 end
 
