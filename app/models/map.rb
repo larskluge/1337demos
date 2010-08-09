6b2083342c0c94be4b6334dcf7b945851f180477
aekym <me@aekym.com>
2010-08-09 15:07:35 +0200

    fix maps#index (without pagination bug)

--- app/models/map.rb
+++ app/models/map.rb
@@ -9,7 +9,7 @@ class Map < ActiveRecord::Base
 	validates_presence_of :name
 	validates_uniqueness_of :name
 
-  named_scope :data_correct, :joins => :demos, :conditions => {:"demos.data_correct" => true}
+  named_scope :data_correct, :joins => :demos, :conditions => {:"demos.data_correct" => true}, :group => "maps.id"
 
 
 
