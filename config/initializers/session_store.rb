a1c215502bf7f602bb8fed5f95fed5183b9e8bbf
aekym <me@aekym.com>
2010-08-24 02:56:30 +0200

    fix deprecation warning: SessionStore#session_key

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,8 +5,8 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :session_key => '1337demos_session',
-  :secret      => '201c9ca2d8956eb62c8a1e91ce124ed28a9cf1571e900a112c2f2d11ebd2acf5ff4ad81deceee166d4564fae762201eb3e48346164497d232ad444d8dda734b9'
+  :key => '1337demos_session',
+  :secret => '201c9ca2d8956eb62c8a1e91ce124ed28a9cf1571e900a112c2f2d11ebd2acf5ff4ad81deceee166d4564fae762201eb3e48346164497d232ad444d8dda734b9'
 }
 
 # Use the database for sessions instead of the cookie-based default,
