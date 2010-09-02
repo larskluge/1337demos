681df36c80efab6137fb05eb2706eafac41126b8
aekym <me@aekym.com>
2010-09-03 00:29:08 +0200

    rails 3 upgrade

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LeetdemosPlatform::Application.config.secret_token = '57e831268343b0e083ff5a38782aff78c0ae4bf3f330b372f1e256238b67c190a25bb85c4d1783131263321411e4e35ca55a247a987c4c541288bdb5f19a90fc'
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,16 +1,8 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key for verifying cookie session data integrity.
-# If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key => '1337demos_session',
-  :secret => '201c9ca2d8956eb62c8a1e91ce124ed28a9cf1571e900a112c2f2d11ebd2acf5ff4ad81deceee166d4564fae762201eb3e48346164497d232ad444d8dda734b9'
-}
+LeetdemosPlatform::Application.config.session_store :cookie_store, :key => '_leetdemos-platform_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
 # (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
-
+# LeetdemosPlatform::Application.config.session_store :active_record_store
