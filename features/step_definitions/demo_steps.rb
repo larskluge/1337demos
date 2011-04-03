80e8c3db86e8a9e2c6072f6c1cb5e85efc3b0f4b
aekym <me@aekym.com>
2011-04-03 21:42:02 +0200

    fix features for ruby 1.9.2 usage

--- features/step_definitions/demo_steps.rb
+++ features/step_definitions/demo_steps.rb
@@ -1,35 +1,29 @@
 Given /^I upload the demo "([^\"]*)" for player "([^\"]*)"$/ do |demo, player|
-  steps %Q{
-    When I go to demo upload
-    And I attach "demofiles/#{demo}" to the "demofile[file]" file field
-    And I press "Upload"
-    Then I should see "Verify information"
-    When I select "#{player}" from "demo[players]"
-    And I choose "yes"
-    And I press "Submit"
-  }
+  When "I go to demo upload"
+  When %Q{I attach "demofiles/#{demo}" to the "demofile[file]" file field}
+  When %Q{I press "Upload"}
+  Then %Q{I should see "Verify information"}
+  When %Q{I select "#{player}" from "demo[players]"}
+  When %Q{I choose "yes"}
+  When %Q{I press "Submit"}
 end
 
 Given /^I upload the demo "([^\"]*)"$/ do |demo|
-  steps %Q{
-    When I go to demo upload
-    And I attach "demofiles/#{demo}" to the "demofile[file]" file field
-    And I press "Upload"
-    Then I should see "Verify information"
-    And I choose "yes"
-    And I press "Submit"
-  }
+  When "I go to demo upload"
+  When %Q{I attach "demofiles/#{demo}" to the "demofile[file]" file field}
+  When %Q{I press "Upload"}
+  Then %Q{I should see "Verify information"}
+  Then %Q{I choose "yes"}
+  Then %Q{I press "Submit"}
 end
 
 Given /^I upload the freestyle demo "([^\"]*)" with title "([^\"]*)" for players "([^\"]*)"$/ do |demo, title, players|
-  steps %Q{
-    When I go to demo upload
-    And I attach "demofiles/#{demo}" to the "demofile[file]" file field
-    And I press "Upload"
-    Then I should see "Verify information"
-    And I fill in "Title" with "#{title}"
-    And I choose "yes"
-  }
+  When %Q{I go to demo upload}
+  When %Q{I attach "demofiles/#{demo}" to the "demofile[file]" file field}
+  When %Q{I press "Upload"}
+  Then %Q{I should see "Verify information"}
+  Then %Q{I fill in "Title" with "#{title}"}
+  Then %Q{I choose "yes"}
 
   players.split(",").each do |player|
     Then "I check \"#{player.strip}\""
