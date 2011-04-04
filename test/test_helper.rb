d93ae1249795cc2768a32c834414ce1ae5764016
aekym <me@aekym.com>
2011-04-04 09:22:56 +0200

    require ruby-debug in test-helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
 require 'factories'
+require 'ruby-debug'
 
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
