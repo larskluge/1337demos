a7d1f60dd897caa42176a5bad8f3dc641af884e6
aekym <me@aekym.com>
2011-05-22 23:09:50 +0200

    rebuild deprecated and removed error_messages_for to pass tests

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -204,5 +204,12 @@ module ApplicationHelper
     end
   end
 
+  def error_messages_for(*records)
+    messages = records.map { |rec| rec.errors.full_messages }.flatten
+    content_tag :ul, messages.map { |msg|
+      content_tag :li, msg, class: 'error'
+    }.join.html_safe
+  end
+
 end
 
