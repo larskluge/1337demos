ab35ac818547e702520a9bcd30c9ff0b8a00fdd4
aekym <me@aekym.com>
2013-09-19 17:33:13 -0400

    Remove exception notifications for now

--- config/initializers/exception_notifier.rb
+++
@@ -1,9 +0,0 @@
-# exception_notifier
-#
-LeetdemosPlatform::Application.config.middleware.use ExceptionNotifier,
-  :sender_address => %("1337demos exception notifier" <exception-notifier@1337demos.com>),
-  :exception_recipients => %w(1337demos@freshthinking.de),
-  :ignore_exceptions => ExceptionNotifier.default_ignore_exceptions + [
-    ActionController::InvalidAuthenticityToken
-  ]
-
