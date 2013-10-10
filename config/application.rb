73dcfafff03c43dd01a942e2d4c58c376d135e9d
aekym <me@aekym.com>
2013-10-10 00:46:01 -0400

    Fix precompilation

--- config/application.rb
+++ config/application.rb
@@ -65,7 +65,7 @@ module LeetdemosPlatform
 
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
-    config.assets.precompile = %w(application.js active_admin.js application.css)
+    config.assets.precompile += %w(application.js active_admin.js application.css)
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
